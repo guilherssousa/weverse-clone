efc33a8e25f0099f8c59cc6c511ce69f3254022e
Guilherme S. Sousa <47110995+guilherssousa@users.noreply.github.com>
2021-09-15 10:28:55 -0300

    initial props _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,22 +1,9 @@
 import Document, {Head, Main, NextScript, Html} from 'next/document';
 // Import styled components ServerStyleSheet
-import {ServerStyleSheet} from 'styled-components';
-
 export default class MyDocument extends Document {
-  static getInitialProps({renderPage}) {
-    // Step 1: Create an instance of ServerStyleSheet
-    const sheet = new ServerStyleSheet();
-
-    // Step 2: Retrieve styles from components in the page
-    const page = renderPage((App) => (props) =>
-      sheet.collectStyles(<App {...props} />),
-    );
-
-    // Step 3: Extract the styles as <style> tags
-    const styleTags = sheet.getStyleElement();
-
-    // Step 4: Pass styleTags as a prop
-    return {...page, styleTags};
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
   }
 
   render() {
@@ -24,7 +11,6 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           <meta charSet="UTF-8"/>
-          {this.props.styleTags}
 
           <link href="/static/global.css" rel="stylesheet"/>
 
