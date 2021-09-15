d64ce2f856b7168dd746bda2144236258a37abe2
Guilherme S. Sousa <47110995+guilherssousa@users.noreply.github.com>
2021-09-15 10:39:24 -0300

    add context initial Props

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,12 +3,13 @@ import Document, {Head, Main, NextScript, Html} from 'next/document';
 import {ServerStyleSheet} from 'styled-components';
 
 export default class MyDocument extends Document {
-  static getInitialProps({renderPage}) {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
     // Step 1: Create an instance of ServerStyleSheet
     const sheet = new ServerStyleSheet();
 
     // Step 2: Retrieve styles from components in the page
-    const page = renderPage((App) => (props) =>
+    const page = ctx.renderPage((App) => (props) =>
       sheet.collectStyles(<App {...props} />),
     );
 
