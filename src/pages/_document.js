c0bd5c72fe8ce2e604b3559322d391ad0064d1fa
Guilherme S. Sousa <47110995+guilherssousa@users.noreply.github.com>
2021-09-15 10:53:15 -0300

    tryna again

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,17 +4,20 @@ import {ServerStyleSheet} from 'styled-components';
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx)
+
+    const renderPage = ctx.renderPage
     // Step 1: Create an instance of ServerStyleSheet
     const sheet = new ServerStyleSheet();
 
     // Step 2: Retrieve styles from components in the page
-    const page = ctx.renderPage((App) => (props) =>
+    const page = renderPage((App) => (props) =>
       sheet.collectStyles(<App {...props} />),
     );
 
     // Step 3: Extract the styles as <style> tags
     const styleTags = sheet.getStyleElement();
+
+    const initialProps = await Document.getInitialProps(ctx)
 
     // Step 4: Pass styleTags as a prop
     return {...initialProps, ...page, styleTags};
@@ -65,3 +68,5 @@ export default class MyDocument extends Document {
     );
   }
 }
+
+MyDocument.displayName = 'Child';
