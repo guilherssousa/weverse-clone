284375d75f043b13ffb35b136638d66d88584979
Guilherme S. Sousa <47110995+guilherssousa@users.noreply.github.com>
2021-09-15 10:35:37 -0300

    comeback to styleTags

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,9 +1,22 @@
 import Document, {Head, Main, NextScript, Html} from 'next/document';
 // Import styled components ServerStyleSheet
+import {ServerStyleSheet} from 'styled-components';
+
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
+  static getInitialProps({renderPage}) {
+    // Step 1: Create an instance of ServerStyleSheet
+    const sheet = new ServerStyleSheet();
+
+    // Step 2: Retrieve styles from components in the page
+    const page = renderPage((App) => (props) =>
+      sheet.collectStyles(<App {...props} />),
+    );
+
+    // Step 3: Extract the styles as <style> tags
+    const styleTags = sheet.getStyleElement();
+
+    // Step 4: Pass styleTags as a prop
+    return {...page, styleTags};
   }
 
   render() {
@@ -11,8 +24,9 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           <meta charSet="UTF-8"/>
-
-          <link href="/static/global.css" rel="stylesheet"/>
+          {this.props.styleTags}
+          
+          <link rel="stylesheet" href="/static/global.css" />
 
           <link
             rel="preload"
